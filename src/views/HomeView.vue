30b79f1d1eb57bb626c3b16a6ad4bbe464a47e30
joao <joao@xbits.com.br>
2022-08-31 14:01:07 -0300

    JVB - Card na home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <RegisterForm />
+    <v-card max-width="374" class="mx-auto my-12">
+      <v-card-title>Card de comida</v-card-title>
+      <v-img
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      ></v-img>
+      <v-card-text> Esse é um bom alimento </v-card-text>
+    </v-card>
   </div>
 </template>
 
